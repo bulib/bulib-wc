7f277194c2f4d7a908b9c5b0205f38f15876a5a3
atla5 <aidans@bu.edu>
2018-10-12 15:46:31 -0400

    libsel+locoso: both components working separately together : ]

--- libsel/libsel.js
+++ libsel/libsel.js
@@ -1,42 +1,49 @@
-import {Slim} from "slim-js";
-import 'slim-js/directives/repeat.js'
+import {LitElement, html} from '@polymer/lit-element';
 
-Slim.tag('libsel',
-  `<div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Dropdown button
-    </button>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <a slim-repeat="options as option" class="dropdown-item" href="/[[code]]">[[name]]</a>
-    </div>
-  </div>
-  `,
-  class LibSel extends Slim {
+class LibSel extends LitElement{
 
-    onBeforeCreated() {
-      this.options = [
-      	{"code":"all","name":"ALL"},
-      	{"code":"mugar-memorial","name":"Mugar Memorial"},
-        {"code":"african-studies","name":"African Studies"},
-        {"code":"medlib", "name":"Alumni Medical"},
-        {"code":"astronomy", "name":"Astronomy"},
-        {"code":"lawlibrary","name":"Fineman and Pappas Law"},
-        {"code":"hgar","name":"Archival Research Center"},
-        {"code":"music","name":"Music Library"},
-        {"code":"management","name":"Frederick S. Pardee Managment Library"},
-        {"code":"pickering-educational","name":"Pickering Educational Resources"},
-        {"code":"sthlibrary","name":"School of Theology"},
-        {"code":"sel","name":"Science and Engineering"},
-        {"code":"stone-science","name":"Stone Science"}
-      ];
+  constructor(){
+    super();
+    this.options = [
+      {"code":"mugar-memorial","name":"SELECT LIBRARY"},
+      {"code":"mugar-memorial","name":"Mugar Memorial"},
+      {"code":"african-studies","name":"African Studies"},
+      {"code":"medlib", "name":"Alumni Medical"},
+      {"code":"astronomy", "name":"Astronomy"},
+      {"code":"lawlibrary","name":"Fineman and Pappas Law"},
+      {"code":"hgar", "name":"Archival Research Center"},
+      {"code":"music","name":"Music Library"},
+      {"code":"management","name":"Frederick S. Pardee Managment Library"},
+      {"code":"pickering-educational","name":"Pickering Educational Resources"},
+      {"code":"sthlibrary","name":"School of Theology"},
+      {"code":"sel","name":"Science and Engineering"},
+      {"code":"stone-science","name":"Stone Science"}
+    ];
+  }
 
-      let url_fragment = "/library/mugar-memorial/services"; //TODO replace with url grab
-      this.selected = this.options[0];
-      for(let o of this.options){
-      	if(url_fragment.includes(o.code)){
-        	this.selected = o;
-        }
-      }
+  static get properties(){
+    return {
+      library: {type: String, notify:true}
     }
   }
-)
+
+  render(){
+    return html`
+      <strong>Library</strong>:
+      <select id="libhours-input" @input=${(e) => this._LibSelectionChanged(e)}}>
+        ${this.options.map((o) => html`<option value="${o.code}">${o.name}</option>`)}
+      </select>
+    `;
+  }
+
+  _LibSelectionChanged(event){
+    let libName = event.currentTarget.value;
+    let element = document.getElementsByTagName("bu-locoso")[0];
+    console.log("before: " + element.getAttribute("library"));
+    element.setAttribute("library", libName);
+    console.log("after: " + element.getAttribute("library"));
+  }
+
+}
+
+customElements.define('bu-libsel', LibSel);
