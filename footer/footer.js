592ae3652e28fa86f0793180be39c6835b42539d
atla5 <aidans@bu.edu>
2018-10-31 10:54:14 -0400

    footer: [bugfix] troubleshoot div spacing issue
    had fixed it on the glitch but not on here. i'm headed in for the night, though.

--- footer/footer.js
+++ footer/footer.js
@@ -23,24 +23,26 @@ class BUFooter extends LitElement {
       </style>
       <footer class="pam">
         <div class="ftr-left">
-            <div id="bu-content">
-              <div class="left txtc bu-logo">
-                <a href="http://www.bu.edu/" title="Boston University Home"><img alt="boston university logo" src="http://www.bu.edu/academics/files/bu-logo.gif"></a>
-                <br /><br />
-                <small><a class="white-link" href="http://www.bu.edu/copyright" title="Copyright">© Copyright ${new Date().getFullYear()}</a></small>
-              </div>
-              <div>
-                <ul class="no-bullet">
-                  <li><a class="white-link" href="http://www.bu.edu/library/" title="Library Home">Library Home</a></li>
-                  <li><a class="white-link" href="https://buprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=BU&sortby=rank&lang=en_US" title="Library Search">Search Content</a></li>
-                  <li><a class="white-link" href="http://askalibrarian.bu.edu/" title="Help">Help</a></li>
-                </ul>
-              </div>
+          <div id="bu-content">
+            <div class="left txtc bu-logo">
+              <a href="http://www.bu.edu/" title="Boston University Home"><img alt="boston university logo" src="http://www.bu.edu/academics/files/bu-logo.gif"></a>
+              <br /><br />
+              <small><a class="white-link" href="http://www.bu.edu/copyright" title="Copyright">© Copyright ${new Date().getFullYear()}</a></small>
             </div>
+            <div>
+              <ul class="no-bullet">
+                <li><a class="white-link" href="http://www.bu.edu/library/" title="Library Home">Library Home</a></li>
+                <li><a class="white-link" href="https://buprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=BU&sortby=rank&lang=en_US" title="Library Search">Search Content</a></li>
+                <li><a class="white-link" href="http://askalibrarian.bu.edu/" title="Help">Help</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div class="ftr-middle"><slot id="sitemap" name="sitemap"></slot></div>
+        <div class="ftr-middle">
+          <div><slot id="sitemap" name="sitemap"></slot></div>
+        </div>
         <div class="ftr-right">
-            <bu-locoso library="${this.library}" link_class="white-link"></bu-locoso>
+          <div><bu-locoso library="${this.library}" link_class="white-link"></bu-locoso></div>
         </div>
       </footer>`;
   }
