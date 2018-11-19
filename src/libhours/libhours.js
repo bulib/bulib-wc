d602a1404ab3be011327b02fcdb892f6fffc1c16
atla5 <aidans@bu.edu>
2018-11-19 13:41:41 -0500

    libhours: initial adjustment to repair libhours
    (but still skeleton)

--- src/libhours/libhours.js
+++ src/libhours/libhours.js
@@ -1,17 +1,35 @@
 import {LitElement, html} from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
 
-class LibHours extends LitElement {
+const libhours = {
+  "mugar-memorial":{"lid":1475,"url":"http://www.bu.edu/library/mugar-memorial/about/hours/"},
+  "african-studies":{"lid":1809,"url":"http://www.bu.edu/library/african-studies/about/hours/"},
+  "astronomy":{"lid":1784,"url":"http://www.bu.edu/library/astronomy/about/hours/"},
+  "music":{"lid":1810,"url":"http://www.bu.edu/library/music/about/hours/"},
+  "pardee":{"lid":1476,"url":"http://www.bu.edu/library/management/about/hours/"},
+  "pickering":{"lid":1783,"url":"http://www.bu.edu/library/pickering-educational/about/hours"},
+  "sel":{"lid":1477,"url":"http://www.bu.edu/library/pickering-educational/about/hours"},
+  "stone":{"lid":1785,"url":"http://www.bu.edu/library/stone-science/about/hours/"}
+};
+
+/** display the hours of operation for a given library */
+class BULibHours extends LitElement {
 
   constructor() {
     super();
-    this.url = "https://www.bu.edu/library/about/hours/";
   }
 
   static get properties() {
     return {
+      /** code for library for which we want to display the hours (used as dict key) */
       library: {type: String, notify:true},
+      /** human-readable name for the currently selected library */
+      libraryName: {type: String},
+      /** the url for the longer-form details for the hours */
       url: {type: String},
-      libHours: {type: String}
+      /** plaintext description of that day's hours */
+      today: {type: String},
+      /** true/false statement of whether the library is open at that exact moment */
+      open: {type: Boolean}
     }
   }
 
@@ -19,51 +37,33 @@ class LibHours extends LitElement {
     let path = window.location.pathname;
     return html`
       <style> .white { color: #000; background: #FFF;} </style>
-      <div class="white">
-        <strong>Library</strong>:
-        <select id="libhours-input" @input=${(e) => this._onLibChange(e)}>
-          <option value="african-studies">African Studies</option>
-          <option value="astronomy">Astronomy</option>
-          <option value="pickering-educational">Pickering</option>
-          <option value="mugar-memorial">Mugar</option>
-        </select>
-      </div>
-      <div>
-        <strong>Library Hours: </strong> &nbsp; <span>${this.libHours}</span>
-        <a target="_blank" href="${this.url}">-></a>
+      <div class="libhours">
+        <div><strong>${this.libraryName}</strong> hours:</div>
+        <div><span id="hours-display">${this.today}</span> <a href="${this.url}">&raquo;</div>
       </div>
     `;
   }
 
-  _onLibChange(event){
+  /** react to changes in the library name */
+  _onLibraryChange(){
     let libName = event.currentTarget.value;
-    console.log(libName);
-    let lid = this._getLidFromPath(libName);
-    //this._getHoursFromLid(lid);
+
+    let lid = libhours[libName]["lid"] || 1475;
+    this.url = libhours[libName]["url"] || "https://www.bu.edu/library/about/hours/";
+
+    //make call to LibCalendar API to get at hours data
     fetch('https://api3.libcal.com/api_hours_today.php?format=json&systemTime=0&iid=1740&lid='+lid,
           {mode:'cors', headers:{'Access-Control-Allow-Origin':'*'}})
       .then(r => {r.json()})
       .then(data => {
         console.log(data[0]);
-        this.url = data[0]["url"];
-        this.libHours = data[0]["rendered"];
+        this.today = data[0]["rendered"];
       })
       .catch(error => {
         console.log(error);
       });
   }
 
-  _getLidFromPath(path){
-    if(path.includes("african-studies")){ return 1809; }
-    else if(path.includes("astronomy")){ return 1784; }
-    else if(path.includes("music")){ return 1810; }
-    else if(path.includes("management")){ return 1476; }
-    else if(path.includes("pickering-educational")){ return 1783; }
-    else if(path.includes("sel")){ return 1477; }
-    else if(path.includes("stone")){ return 1785; }
-    else{ return 1475; /* 'mugar-memorial' */ }
-  }
-
 }
 
-customElements.define('lib-hours', LibHours);
+customElements.define('bulib-hours', BULibHours);
