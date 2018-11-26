cac93e94d6e138f927f536c8076ebbe2bd226041
atla5 <aks5238@rit.edu>
2018-11-26 18:20:53 +0000

    footer: [bugfix] adjust footer.js css link

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -17,8 +17,8 @@ class BULFooter extends LitElement {
 
   render() {
     return html`
-      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v1.1/assets/css/common.min.css">
-      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v1.1/src/footer/footer.min.css">
+      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v1.3/assets/css/common.min.css">
+      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v1.3/src/footer/footer.min.css">
       <style>
         :host h3, h3 { margin-top: 0px; margin-bottom: 0px; }
         ul, ol { margin-top: 5px; margin-bottom: 5px; }
