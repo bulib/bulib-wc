d938a08275dbb3737762bb2e67744741e5e6ffa3
atla5 <aks5238@rit.edu>
2018-12-11 18:40:05 +0000

    search: update '#search_query_input' styling

--- src/search/search.js
+++ src/search/search.js
@@ -71,7 +71,6 @@ class BULSearch extends LitElement {
     `;
     
     return html`
-      <style type="text/css"> #search_query_input { min-width: 200px; } </style>
       <div id="bulib-search">
         <input id="search_query_input" type="text" placeholder="${this.selected["placeholder"]}" @keypress="${(e) => this._handleSearchEnter(e)}"></input>
         ${optionsDisplay}
@@ -125,6 +124,10 @@ class BULSearch extends LitElement {
         if(option.value === this.selected["value"]){ option.selected = "selected"; }
       }
     }
+    
+    // set the input box width based on the placeholder
+    let inputElem = this.querySelector("#search_query_input");
+    inputElem.setAttribute('size',inputElem.getAttribute('placeholder').length);
   }
 
   /** perform a search for the input query on the selected database */
