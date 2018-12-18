a7778f089f395cb78870163ea69d69667ecd795d
atla5 <aks5238@rit.edu>
2018-12-18 21:24:55 +0000

    WEB-36: add multi-select and refactor

--- src/_helpers/browser_compatibility.js
+++ src/_helpers/browser_compatibility.js
@@ -1,3 +1,5 @@
+var debug = true;
+
 var doesBrowserSupportWebComponents = function(){
   /** determine the browser the code is being run on
    * note: thanks to [stackoverflow](https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser/9851769) for the implementation
@@ -8,21 +10,23 @@ var doesBrowserSupportWebComponents = function(){
   return user_agent_supports_web_components;
 };
 
-var loadWebComponentInto = function(tagname, replacedElement){
+var loadWebComponentInto = function(tagname, replacedElements){
   var user_agent_supports_web_components = doesBrowserSupportWebComponents();
-  if(user_agent_supports_web_components){
-    // show the component and hide its replacement
-    console.log("user_agent_supports_web_components! :)");
-    replacedElement.style.visibility = "hidden";
-  }else{
-    // hide the component itself
-    console.log("!user_agent_supports_web_components. :(");
-    console.log(tagname);
-    var lsElements = document.getElementsByTagName(tagname);
-    console.log(lsElements);
-    for(var i=0; i<lsElements.length; i++){
-      lsElements[i].style.visibility = "hidden";
-    }
+  
+  // determine which elements to hide
+  var elementsToHide = user_agent_supports_web_components 
+    ? replacedElements // hide replaced elements if the web components worked,
+    : document.getElementsByTagName(tagname) // hide the contents of the custom tag if they don't (avoid showing 'slotted' coni)
+  ; 
+  
+  // do the hiding
+  for(var i=0; i<elementsToHide.length; i++){
+    elementsToHide[i].style.visibility = "hidden";
+  }
+  
+  // log the result
+  if(debug){
+    var supportsMessage = user_agent_supports_web_components ? ": ]" : ":(";
+    console.log("user_agent_supports_web_components: " + supportsMessage);
   }
 };
-module.exports = { loadWebComponentInto:loadWebComponentInto };
