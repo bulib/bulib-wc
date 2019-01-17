78301b766766c11c9ddd0707c7094df0d1cb5154
atla5 <aks5238@rit.edu>
2019-01-17 19:01:21 +0000

    WEB-58: update footer responsive breakpoints

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -83,7 +83,7 @@ class BULFooter extends LitElement {
     // render the main content of the component
     return html`
       <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v2.6.4/assets/css/common.min.css">
-      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v2.6.4/src/footer/footer.css">
+      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/bulib/bulib-wc@footer-v2.6.5/src/footer/footer.css">
       <style>
         /* firefox fix to stop the 'Follow Us' from becoming centered */
         ::slotted(h3) { text-align: left; }
