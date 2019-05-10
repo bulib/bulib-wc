e6b9ccbaec4d17f435e2e61051ddeb655b6046ca
atla5 <aks5238@rit.edu>
2019-05-10 16:30:07 +0000

    WEB-119: dummy index.js, raw/blank examples to main-content

---
+++ docs/index.js
@@ -0,0 +1,12 @@
+console.log("bulib-docs) 'index.js' loaded");
+
+// import all of the html usages 
+// import {footer} from '/src/footer/footer.html';
+
+// once the page loads, grab the anchor and determine which component to show
+//document.getElementById("main-element").innerHTML = footer;
+
+  // populate the '#main-element' with the content imported from above
+  
+  // ideally bold the secondary header link
+  
