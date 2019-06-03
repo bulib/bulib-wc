57554bab648e118a36a27e9fe178bd9776704e39
atla5 <aidans@bu.edu>
2019-06-03 13:52:06 -0400

    WEB-84: reconfigure rollup.config, 'npm run' scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,22 @@
-import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config';
+// import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config';
+import resolve from 'rollup-plugin-node-resolve';
+import minify from 'rollup-plugin-babel-minify';
 
-export default createDefaultConfig({ input: './src/index.html' });
+export default {
+  input: 'src/index.js',
+  inlineDynamicImports: true,
+  output: [
+    {
+      name: 'bulibwc',
+      file: 'dist/bundle.umd.js',
+      format: 'umd',
+      globals: []
+    }
+  ],
+  watch: { include: 'src/*' },
+  external: ['lit-element', '@open-wc/polyfills-loader', 'lit-html'],
+  plugins: [
+    resolve(),
+    minify()
+  ]
+};
