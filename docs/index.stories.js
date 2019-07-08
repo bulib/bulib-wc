74f82567066ea890b6a1ec12ea397ce37d291c55
atla5 <aidans@bu.edu>
2019-07-08 16:21:00 -0400

    WEB-145: cta+card css, reorg 'stories'
    composites, components, patterns

--- docs/index.stories.js
+++ docs/index.stories.js
@@ -1,15 +1,24 @@
 import '../src/index';
 import { storiesOf, withKnobs, withClassPropertiesKnobs } from '@open-wc/demoing-storybook';
 
-import {ensemble_demo, search_demo, hours_demo, locoso_demo, header_demo, footer_demo} from './demo_html';
+import {ensemble_demo, search_demo, hours_demo, locoso_demo, header_demo, footer_demo, card_demo, cta_demo} from './demo_html';
 
-storiesOf('bulib-wc', module)
+storiesOf('composites', module)
   .addDecorator(withKnobs)
   .add('ensemble',     () => ensemble_demo)
+  .add('bulib-header',  () => header_demo)
+  .add('bulib-footer', () => footer_demo)
+;
+
+storiesOf('components', module)
+  .addDecorator(withKnobs)
 //.add('bulib-search', () => withClassPropertiesKnobs(BULibSearch))
   .add('bulib-search', () => search_demo)
   .add('bulib-hours',  () => hours_demo)
   .add('bulib-locoso', () => locoso_demo)
-  .add('bulib-header',  () => header_demo)
-  .add('bulib-footer', () => footer_demo)
+;
+
+storiesOf('patterns', module)
+  .add('card', () => card_demo)
+  .add('call-to-action', () => cta_demo)
 ;
