1d840d44e25158bbbca92d8c6398418cfd19dd31
atla5 <aidans@bu.edu>
2019-08-02 16:31:58 -0400

    Revert "WEB-172: unpkg deploy (search-v1.9)"
    This reverts commit 1ce75265a0328fac2b60b524f72b6908cb21ebbf.

--- src/search/search.js
+++ src/search/search.js
@@ -1,4 +1,4 @@
-import {LitElement, html, css} from 'https://unpkg.com/lit-element@2.2.0/lit-element.js?module';
+import {LitElement, html, css} from 'lit-element/lit-element'; //'https://unpkg.com/lit-element@2.2.0/lit-element.js?module';
 const ENTER_KEY_VALUE = 13;
 const default_to_just_primo = true;
 
@@ -217,5 +217,3 @@ export default class BULibSearch extends LitElement {
   }
 
 }
-
-window.customElements.define("bulib-search",BULibSearch); 
