5c839aa06320ec1dbb739cca435da770143f2591
atla5 <aidans@bu.edu>
2019-08-02 13:17:41 -0400

    WEB-172: adjust padding, benton-font, card width

--- scripts/bundle_css.sh
+++ scripts/bundle_css.sh
@@ -8,7 +8,7 @@ scss ./src/header/mobile-nav.scss >> ./src/header/header.css;
 # bundle css into single 'shared' css file
 mkdir dist || true;
 cat ./node_modules/material-icons/css/material-icons.min.css > $CSS_BUNDLE_FILEPATH;
-
+cat assets/css/benton-font.css >> $CSS_BUNDLE_FILEPATH;
 cat assets/css/common.css >> $CSS_BUNDLE_FILEPATH;
 cat assets/css/card.css >> $CSS_BUNDLE_FILEPATH;
 cat assets/css/cta.css >> $CSS_BUNDLE_FILEPATH;
