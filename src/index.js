f432a4dee181983127091e01dda31510d2737b72
atla5 <aidans@bu.edu>
2019-08-12 18:43:28 -0400

    WEB-171: rm -rf ./src/dropdown/

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import loadPolyfills from '@open-wc/polyfills-loader';
 loadPolyfills().then(() => {
   import ('./select/bulib-select');
   import ('./card/bulib-card');
-  import ('./dropdown/bulib-dropdown');
   import ('./libhours/bulib-hours');
   import ('./search/bulib-search');
   import ('./locoso/bulib-locoso');
