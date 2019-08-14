87962de6b44b6322cdadbf50baa5e4ce4ac83c9c
atla5 <aidans@bu.edu>
2019-08-14 11:50:54 -0400

    WEB-180: move 'Promise.all([])' piece, add log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 window.WebComponents.waitFor(() => {
-  Promise.all([
-    import ('./select/bulib-select'),
-    import ('./card/bulib-card'),
-    import ('./libhours/bulib-hours'),
-    import ('./search/bulib-search'),
-    import ('./locoso/bulib-locoso'),
-    import ('./footer/bulib-footer')
-  ]);
+  console.log("polyfills loaded (?)");
+
+  import ('./select/bulib-select');
+  import ('./card/bulib-card');
+  import ('./libhours/bulib-hours');
+  import ('./search/bulib-search');
+  import ('./locoso/bulib-locoso');
+  import ('./footer/bulib-footer');
 });
