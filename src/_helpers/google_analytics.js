34fd07a1801c87ed8004e04d295ad9e9c7fddfda
atla5 <aidans@bu.edu>
2019-08-16 13:02:04 -0400

    WEB-181: initialize[GA, GTAG], sites.html

--- src/_helpers/google_analytics.js
+++ src/_helpers/google_analytics.js
@@ -5,6 +5,23 @@ function logSendGAEvent(message){
   if(DEBUG_ANALYTICS){ console.log("google_analytics_helper) " + message); }
 }
 
+export function initializeGA(ga_tracking_id){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', ga_tracking_id, 'auto');
+  ga('send', 'pageview');
+}
+
+export function initializeGTAG(ga_tracking_id){
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', ga_tracking_id);  
+}
+
 export function sendGAEvent(eventName, action, label){
   logSendGAEvent(`request made to sendGAEvent('${eventName}', '${action}', '${label}')`);
   if(window.ga){ 
