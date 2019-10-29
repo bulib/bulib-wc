765e6f63ccb9d8d400fd5f415c610a2bb894036d
atla5 <aidans@bu.edu>
2019-10-29 14:44:33 -0400

    WEB-194: [docs] add notes clarifying background, future

--- sites/wordpress/search_redirector_override.js
+++ sites/wordpress/search_redirector_override.js
@@ -1,10 +1,13 @@
 /**
  * NOTE: this is a bit of a quick-fix solution to resolve a production issue WEB-194.
  * 
- * - the following hijacks the search form submission on the home page, preventing 
- *   a call from being made to https://github.com/bu-ist/bu-library-theme/blob/master/search-redirector.php,
- *   which, if allowed, results in 403 errors due to 'Web Application Firewall' configurations by IS&T
- * - it
+ *  - the following hijacks the search form submission on the home page, preventing 
+ *    a call from being made to https://github.com/bu-ist/bu-library-theme/blob/master/search-redirector.php
+ *    which, if allowed, results in 403 errors.
+ *  - we know from a previous issue (WEB-83) that this is likely due to the 'Web Application Firewall' 
+ *    configurations by IS&T, but redressing the issue has not been quick, and we don't want/need the redirector
+ *    going forward really, anyway 
+ *  - this patche should be replaced once the homepage is replaced by the '<bulib-search>' component.
  */
 
 let doBULSCatalogSearch = function(query){
