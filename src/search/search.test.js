d303038c8ca42c5a71a415c6ac084ed61c8ae251
atla5 <aidans@bu.edu>
2019-12-20 15:44:04 -0500

    WEB-248: [bugfix] (-) try setting variable for headless_chrome (karma)
    working off of the following https://discuss.circleci.com/t/env-variables/30714

--- src/search/search.test.js
+++ src/search/search.test.js
@@ -3,7 +3,7 @@ import { html, fixture, expect } from '@open-wc/testing';
 import './bulib-search';
 
 describe('bulib-search', () => {
-  it('to effectively load as a web component (renderRoot.innerHTML)', async () => {
+  it('to effectively load as a web component', async () => {
     const el = await fixture(html`<bulib-search></bulib-search>`);
     expect(el.renderRoot.innerHTML).not.to.be.undefined;
   });
