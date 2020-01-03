85a4d8fa3d4aeb95304d9822081409d678dfe78d
atla5 <aidans@bu.edu>
2020-01-03 10:27:04 -0500

    [bugfix] copy/pasta error leading to double 'library/' links

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -8,12 +8,12 @@ const sitemap_values = {
     "wordpress":{
     "header":"About the Libraries",
     "links":[
-      {"title":"InterLibrary Borrowing","href":"https://www.bu.edu/library/library/services/ill/"},
-      {"title":"Course Reserves",       "href":"https://www.bu.edu/library/library/services/reserves/"},
-      {"title":"For Alumni",            "href":"https://www.bu.edu/library/library/services/alumni/"},
-      {"title":"Staff Directory",       "href":"https://www.bu.edu/library/library/about/who-we-are/staff-directory/"},
-      {"title":"Maps & Floorplans",     "href":"https://www.bu.edu/library/library/about/maps-floorplans/"},
-      {"title":"For Faculty",           "href":"https://www.bu.edu/library/library/services/for-faculty/"}
+      {"title":"InterLibrary Borrowing","href":"https://www.bu.edu/library/services/ill/"},
+      {"title":"Course Reserves",       "href":"https://www.bu.edu/library/services/reserves/"},
+      {"title":"For Alumni",            "href":"https://www.bu.edu/library/services/alumni/"},
+      {"title":"Staff Directory",       "href":"https://www.bu.edu/library/about/who-we-are/staff-directory/"},
+      {"title":"Maps & Floorplans",     "href":"https://www.bu.edu/library/about/maps-floorplans/"},
+      {"title":"For Faculty",           "href":"https://www.bu.edu/library/services/for-faculty/"}
     ]
   },"askalibrarian":{
     "header":"Ask A Librarian",
