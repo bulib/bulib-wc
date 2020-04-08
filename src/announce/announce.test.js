834386c5471cf4cb107ef0faa41e27640562cf82
atla5 <aidans@bu.edu>
2020-04-07 22:41:21 -0400

    WEB-332: test whether 'bulib-announce' updates with API

--- src/announce/announce.test.js
+++ src/announce/announce.test.js
@@ -1,7 +1,7 @@
 import { html, fixture, expect } from '@open-wc/testing';
 
 import './bulib-announce';
-import { elementUpdated } from '@open-wc/testing-helpers';
+import { aTimeout, elementUpdated } from '@open-wc/testing-helpers';
 
 const elementIsHidden = (el) => { return el.offsetHeight == 0; }
 
@@ -67,13 +67,21 @@ describe('bulib-announce', () => {
     expect(innerMessage.innerText).to.include("custom message");
   });
 
-  it("the rendered custom message does NOT get replaced when 'prevent_action' is there, regardless of the api-enabled code", async () => {
+  it('the custom message is replaced when a an api-enabled "code" is given', async () => {
+    const el = await fixture(html`<bulib-announce code="testing" message="this is a custom message"></bulib-announce>`);
+    
+    // give time for the API to get called, and then check that the message is different than the above
+    await aTimeout(800);
+    let innerMsg = el.querySelector("div.announce-banner > .message");
+    expect(innerMsg.innerText).not.to.include("custom message");
+  });
+
+  it("the custom message is NOT replaced when 'prevent_action' is there, regardless of the api-enabled code", async () => {
     let el = await fixture(html`<bulib-announce debug code="primo" message="custom message" prevent_action></bulib-announce>`);
     
-    // click on the dismiss button and wait for the element to update
-    let innerMsgPrevent = el.querySelector("div.announce-banner > .message");
-    
-    // assert that the innerHTML changes and that the banner becomes hidden
-    expect(innerMsgPrevent.innerText).to.include("custom message");
+    // give time for the API to get called, but expect it not to have been
+    await aTimeout(800);
+    let innerMsg = el.querySelector("div.announce-banner > .message");
+    expect(innerMsg.innerText).to.include("custom message");
   });
 });
