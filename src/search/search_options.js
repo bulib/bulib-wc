82c3c2dbc1f27f4141571df9590939bf61df03c2
atla5 <aidans@bu.edu>
2020-08-07 15:34:25 -0400

    header: font sizes, dropdowns, announcement-banner

--- src/search/search_options.js
+++ src/search/search_options.js
@@ -6,7 +6,7 @@ export const search_options = [
   {"value":"primo",       "name":"BU Libraries Search",      "placeholder": "Search library resources",         "domain":"https://buprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=BU&institution=BOSU&search_scope=default_scope&highlight=true&lang=en_US&query=any,contains,"},
   {"value":"wp",          "name":"Main Site",                "placeholder": "Search library info/services",     "domain":"https://search.bu.edu/?site=www.bu.edu%2Flibrary&q="},
   {"value":"industries",  "name":"Industry Survey Locator",  "placeholder": "Search for industry surveys",      "domain":"https://buprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=ISL&institution=BOSU&search_scope=default_scope&highlight=true&lang=en_US&query=any,contains,"},
-  {"value":"guides",      "name":"Library Guides",           "placeholder": "Search research guides",           "domain":"http://library.bu.edu/srch.php?q="},
+  {"value":"guides",      "name":"Research Guides",          "placeholder": "Search research guides",           "domain":"http://library.bu.edu/srch.php?q="},
   {"value":"openbu",      "name":"Open BU",                  "placeholder": "Search BU Digital Collections",    "domain":"https://open.bu.edu/discover?query="},
   {"value":"db",          "name":"BU Databases",             "placeholder": "Search for databases",             "domain":"http://library.bu.edu/az.php?q="},
   {"value":"worldcat",    "name":"OCLC WorldCat",            "placeholder": "BU Libraries Search",              "domain":"https://bu.on.worldcat.org/search?queryString="},
