453e1979aa4b7ccd32b05aeaf2a461bbea086bb1
mwardlb <mrward@bu.edu>
2020-09-08 14:17:16 -0400

    rtc: remove spring2020 from search options

--- src/search/search_options.js
+++ src/search/search_options.js
@@ -1,7 +1,6 @@
 /** data on the overall search sources we have available to search on */
 export const search_options = [
   {"value":"help",        "name":"Help Topics",              "placeholder": "Search for help topics",           "domain":"https://askalibrarian.bu.edu/search/?t=0&q="},
-  {"value":"help-spr20",  "name":"Spring 2020 FAQs",         "placeholder": "Search for Spring 2020 FAQs",      "domain":"https://askalibrarian.bu.edu/spring2020/search/?t=0&g=7265&q"},
   {"value":"pardee-help", "name":"Business FAQs",            "placeholder": "Search for business help topics",  "domain":"https://askalibrarian.bu.edu/businessFAQs/search/?t=0&q="},
   {"value":"primo",       "name":"BU Libraries Search",      "placeholder": "Search library resources",         "domain":"https://buprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=BU&institution=BOSU&search_scope=default_scope&highlight=true&lang=en_US&query=any,contains,"},
   {"value":"wp",          "name":"Main Site",                "placeholder": "Search library info/services",     "domain":"https://search.bu.edu/?site=www.bu.edu%2Flibrary&q="},
